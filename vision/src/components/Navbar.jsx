1a3213dd7f31b6b9f43de1779a1563ac6642c41b
SHARANYASWANT <sharanyaswant.06@gmail.com>
2024-08-13 19:00:12 +0530

    modified:   vision/package-lock.json 	modified:   vision/src/components/Navbar.css 	modified:   vision/src/components/Navbar.jsx

--- vision/src/components/Navbar.jsx
+++ vision/src/components/Navbar.jsx
@@ -5,19 +5,17 @@ import logo from '../assets/logo.png'
 
 const NavBar = () => {
   return (
-    <nav className="navbar">
-      <div>
-        <img src={logo} alt="logo" className="logo"/>
-      </div>
-      <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Fashion</a></li>
-        <li><a href="#">Gadgets</a></li>
-        <li><a href="#">Lifestyle</a></li>
-        <li><a href="#">Video</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
+    <div className='container'>
+      <nav className="navbar">
+        <div>
+          <img src={logo} alt="logo" className="logo"/>
+        </div>
+        <ul className="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
