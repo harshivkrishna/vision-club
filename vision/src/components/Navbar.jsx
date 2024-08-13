ed6d65c8e5578242bd18324d13806c76fb3bb9fd
SHARANYASWANT <sharanyaswant.06@gmail.com>
2024-08-13 18:27:58 +0530

    modified:   vision/src/App.jsx 	new file:   vision/src/components/Navbar.css 	modified:   vision/src/components/Navbar.jsx

--- vision/src/components/Navbar.jsx
+++ vision/src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import Logo from ''
-const Navbar = () => {
-  return (
-    <>
-        
-    </>
-  )
-}
+// NavBar.js
+import React from 'react';
+import './NavBar.css';
 
-export default Navbar
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <a href="#">AVISION</a>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Fashion</a></li>
+        <li><a href="#">Gadgets</a></li>
+        <li><a href="#">Lifestyle</a></li>
+        <li><a href="#">Video</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
