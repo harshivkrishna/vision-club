a8021732524240be13821c151552ca5cdba99fcf
harshivkrishnam <harshivkrishnam@gmail.com>
2024-08-14 06:59:32 +0530

    navbar responsive

--- vision/src/components/Navbar.jsx
+++ vision/src/components/Navbar.jsx
@@ -1,18 +1,28 @@
-// NavBar.js
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
-import logo from '../assets/logo.png'
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleNavLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
     <div className='container'>
       <nav className="navbar">
-        <div>
-          <img src={logo} alt="logo" className="logo"/>
-        </div>
-        <ul className="nav-links">
+        <img src={logo} alt="logo" className="logo" />
+        <i
+          className={showLinks ? 'ri-close-line menu-btn' : 'ri-menu-line menu-btn'}
+          onClick={toggleNavLinks}
+        ></i>
+        <ul className={`nav-links ${showLinks ? 'show' : ''}`}>
           <li><a href="#">Home</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">About</a></li>
           <li><a href="#">Contact</a></li>
+          <button>Download</button>
         </ul>
       </nav>
     </div>
